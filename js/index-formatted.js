a6498f7cbdd7daa2e34e86a21f14343755408dff
EugeneYuriev <juriev_gek@mail.ru>
2016-05-17 16:22:50 +0300

    Исправлены замечания - Шаг 2

---
+++ js/index-formatted.js
@@ -0,0 +1,48 @@
+var site_shadow = document.querySelector(".site-shadow");
+
+var link = document.querySelector(".feedback");
+var popup = document.querySelector(".feedback-popup");
+var close = popup.querySelector(".feedback-close-btn");
+
+var form = popup.querySelector("feedback-form");
+var name = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+
+var storage = localStorage.getItem("email");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("feedback-popup-show");
+  site_shadow.classList.add("site-shadow-show");
+
+  if (storage) {
+    name.value = storage;
+    email.focus();
+  } else {
+    name.focus();
+  }
+
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("feedback-popup-show");
+  site_shadow.classList.remove("site-shadow-show");
+});
+
+form.addEventListener("submit", function(event) {
+  if (!name.value || !email.value) {
+    event.preventDefault();
+    console.log("Нужно ввести имя и почту");
+  } else {
+    localStorage.setItem("name", login.value);
+  }
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("feedback-popup-show")) {
+      popup.classList.remove("feedback-popup-show");
+    }
+  }
+});
